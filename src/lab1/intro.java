c05a3c3e8ea93cf2c38661e38e82668517303ee8
mind <minelove123_@hotmail.com>
2018-11-26 14:34:45 +0700

    lab 2 : Variables

---
+++ src/lab1/intro.java
@@ -0,0 +1,27 @@
+package lab1;
+
+public class intro {
+    //to do code here
+    public static void main(String[] args) {
+
+        System.out.println("sasitorn ohcuaiy");
+        System.out.println("minelove123_@hotmail.com");
+        System.out.println("is");
+        System.out.println("Rmutsv");
+
+
+
+
+
+
+
+
+
+
+
+    }//main
+
+
+}//class
+
+
---
+++ src/lab2/ex1.java
@@ -0,0 +1,37 @@
+package lab2;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+// integer
+        int a;
+        a = 10;
+        System.out.println(a);
+        int b= 20;
+        System.out.println(b);
+        int c= a+b;
+        System.out.println("ผลรวมจอง a และ  b เท่ากับ\t"+c);
+//real number
+double w = 5.5;
+System.out.println(w);
+double o = 5.5 *10;
+System.out.println(w);
+//w = w *10;
+        w*=10; // w =w*10;
+System.out.println(w);
+
+//character
+        char d ='#';
+System.out.println(d);
+
+//String
+        String str = "sasitorn ohcuai";
+        System.out.println("my name is " +str);
+        System.out.println(str.length());
+        System.out.println(str.toUpperCase());
+
+
+    }//main
+
+
+}//class
