fb9d744edee8dd59be283f6309b707d6ce7fa3fa
mind <minelove123_@hotmail.com>
2018-12-17 15:13:28 +0700

    lab 4 : opeatorn

---
+++ src/lab4/ex3.java
@@ -0,0 +1,17 @@
+package lab4;
+
+public class ex3 {
+    public static void main(String[] args) {
+        int num = A(5, 5);
+    }
+
+    public static int A(int a, int b) {
+        return a * b;
+    }
+
+    public static double A(double a, double b) {
+        return a * b;
+    }
+} //   class
+
+
