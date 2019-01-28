6539f60e5d00555c25f80699bd004890c86e0354
mind <minelove123_@hotmail.com>
2019-01-28 14:18:31 +0700

    lab 7

---
+++ src/lab7/StudentApp.java
@@ -0,0 +1,23 @@
+package lab7;
+
+public class StudentApp {
+    public static void main(String[] args) {
+        //object creation
+        //การสร้าง object
+        Student std1=new Student();
+    System.out.println(std1.getName());
+    std1.setName("Boy");
+        System.out.println(std1.getName());
+        System.out.println(std1.getGender());
+        std1.setGender('M');
+        System.out.println(std1.getGender());
+        std1.setId("358411110045");
+        System.out.println(std1.getId());
+        ///////////////
+        Student std2=new Student("358411110045","Girl",'F');
+
+
+        System.out.println(std1.getName());
+        System.out.println(std1.getName());
+    }
+}//class
